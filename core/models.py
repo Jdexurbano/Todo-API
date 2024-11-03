721d610a8a61302ebf5c5fb1056e46e5cdd72d8a
Jdexurbano <dexterurbano25@gmail.com>
2024-11-03 20:14:27 +0800

    create task model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete = models.CASCADE, related_name = 'tasks')
+    title = models.CharField(max_length = 100)
+    content = models.CharField(max_length = 100)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now = True)
+
+    def __str__(self):
+        return str(f"{self.user} {self.title}")
