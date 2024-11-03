da4e760deea425a95f83cbb2abbcf6289bbcbe19
Jdexurbano <dexterurbano25@gmail.com>
2024-11-03 15:29:10 +0800

    add user registration and login view

--- core/api/views.py
+++ core/api/views.py
@@ -1,9 +1,25 @@
-from django.http import JsonResponse
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer
 
-def getRoutes(request):
+class UserRegistrationView(APIView):
+    
+    def post(self,request):
+        serializer = UserRegistrationSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message":"user created succesfully"},status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
-    routes = [
-        'api/',
-    ]
+class UserLoginView(APIView):
 
-    return JsonResponse(routes, safe=False)
+    def post(self,request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username = username, password = password)
+        if user:
+            return Response({"message":"succesfully login"}, status = status.HTTP_200_OK)
+        return Response({"error":"invalid credentials"}, status = status.HTTP_400_BAD_REQUEST)
