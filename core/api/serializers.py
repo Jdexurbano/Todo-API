71d54345911d90d156a48e3a03aa7f4e83d17be7
Jdexurbano <dexterurbano25@gmail.com>
2024-11-04 14:51:15 +0800

    add password confirmation

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -4,18 +4,25 @@ from rest_framework import serializers
 
 #serializer for user registration
 class UserRegistrationSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only = True,required = True)
+    password1 = serializers.CharField(write_only = True,required = True)
+    password2 = serializers.CharField(write_only = True,required = True)
 
     class Meta:
         model = User
-        fields = ['username','password','email']
+        fields = ['username','password1','password2','email']
+
+    def validate(self, attrs):
+        #check if the password is match
+        if attrs['password1'] != attrs['password2']:
+            raise serializers.ValidationError({"password":"passwords do not match"})
+        return attrs
     
     def create(self, validated_data):
         user = User(
             username = validated_data['username'],
             email = validated_data['email']
         )
-        user.set_password(validated_data['password'])
+        user.set_password(validated_data['password1'])
         user.save()
         return user
 
